3574fa8f78c579a34246ca3d2b7ef24301e8ce3b
Mateus Dantas <mateuscd10@gmail.com>
2013-12-05 21:50:01 -0300

    mateus

--- kernel/constants.php
+++ kernel/constants.php
@@ -1,5 +1,11 @@
 <?php
 
 define("USER_COLUMNS", "id|username|password|salt|email|access_level");
+define("WRONG_PASSWORD", 1);
+define("WRONG_USERNAME", 2);
+define("USER_ALREADY_EXISTS",3);
+define("INVALID_USER",4);
+define("INVALID_EMAIL",5);
+define("INVALID_PASSWORD",6);
 
 ?>
