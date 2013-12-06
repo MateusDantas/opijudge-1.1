18e407c85fc91ca320d9ef592e32378d6af10cdb
Mateus Dantas <mateuscd10@gmail.com>
2013-12-06 01:34:08 -0300

    :(

--- kernel/class.File.php
+++ kernel/class.File.php
@@ -5,6 +5,10 @@ class File
 	private $path = null;
 	private $file_name = null;
 	private $extension = null;
+	
+	/**
+	 * Constructor Set path, file_name and extension
+	 */
 	public function __construct( $filename )
 	{
 		$this->path = $this->get_path_from_str($filename);
@@ -12,26 +16,42 @@ class File
 		$this->extension = $this->get_extension_from_str($filename);
 	}
 	
+	/**
+	 * @return Full File Path
+	 */ 
 	public function get_total_path()
 	{
 		return $this->path . $this->file_name . $this->extension;
 	}
 	
+	/**
+	 * @return File Path
+	 */
 	public function get_path()
 	{
 		return $path;
 	}
 	
+	/**
+	 * @return File name
+	 */
 	public function get_file_name()
 	{
 		return $file_name;
 	}
 	
+	/**
+	 * @return File Extension
+	 */
 	public function get_extension()
 	{
 		return $extension;
 	}
 	
+	/**
+	 * @param total_path Full path from some file
+	 * @return File path
+	 */
 	public function get_path_from_str($total_path)
 	{
 		$array_path = explode("/",$total_path);
@@ -42,6 +62,10 @@ class File
 		return $path_str;
 	}
 	
+	/**
+	 * @param total_path Full path from some file
+	 * @return File name
+	 */
 	public function get_file_name_from_str($total_path)
 	{
 		$array_path = explode("/",$total_path);
@@ -50,6 +74,10 @@ class File
 		return $file_name_ext;
 	}
 	
+	/**
+	 * @param total_path Full path from some file
+	 * @return File extension
+	 */
 	public function get_extension_from_str($total_path)
 	{
 		$file_total_name = $this->get_file_name_from_str($total_path);
@@ -62,6 +90,10 @@ class File
 		return $array_file_name[1];
 	}
 	
+	/**
+	 * @param file_content File content
+	 * @return TRUE if file was successfuly written or FALSE otherwise
+	 */
 	public function write($file_content)
 	{
 		$total_path = $this->get_total_path();
@@ -75,6 +107,12 @@ class File
 		return TRUE;
 	}
 	
+	/**
+	 * Write some content to some file
+	 * @param file_total_path Path from some file
+	 * @param file_content File content
+	 * @return TRUE if file was successfuly written or FALSE otherwise
+	 */
 	public function write_to_file($file_total_path, $file_content)
 	{
 		$handle = fopen($file_total_path,'w+b');
@@ -87,6 +125,9 @@ class File
 		return TRUE;
 	}
 	
+	/**
+	 * Read content from this file
+	 */
 	public function read()
 	{
 		$total_path = $this->get_total_path();
@@ -96,6 +137,10 @@ class File
 		return $data;
 	}
 	
+	/**
+	 * Read content from some file
+	 * @param file_total_path Total path from some file
+	 */
 	public function read_from_file($file_total_path)
 	{
 		$handle = fopen($file_total_path,'r');
@@ -104,6 +149,11 @@ class File
 		return $data;
 	}
 	
+	/**
+	 * Move from some file to this file
+	 * @param file_name Total path from some file
+	 * @return TRUE if successfull or FALSE otherwise
+	 */
 	public function move_from($file_name)
 	{
 		$file_content = $this->read_from_file($file_name);
@@ -112,6 +162,11 @@ class File
 		return TRUE;
 	}
 	
+	/**
+	 * Move from this file to some file
+	 * @param file_name Total path from some file
+	 * @return TRUE if successfull or FALSE otherwise
+	 */
 	public function move_to($file_name)
 	{
 		$file_content = $this->read();
