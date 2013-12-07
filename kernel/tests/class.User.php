f0397662269caae9f81dc68d7769bfb86a04870a
Rafael Perrella <rafael.clp@hotmail.com>
2013-12-07 01:09:41 -0300

    login, get, update tests created

--- kernel/tests/class.User.php
+++ kernel/tests/class.User.php
@@ -5,6 +5,8 @@ require_once("/../class.User.php");
 
 class UserTest
 {
+	private $rafaelclp_id;
+
 	function register()
 	{
 		MySQL::query("DELETE FROM `user`");
@@ -16,9 +18,9 @@ class UserTest
 			$user[$i]->password = "rafael";
 		}
 
-		$user[0](array("username" => generate_random_string(MIN_USER_LENGTH, MIN_USER_LENGTH, USER_OPTIONAL_ALPHABET), "email" => "rafael.clp@hotmail.com"));
-		$user[1](array("username" => generate_random_string(MIN_USER_LENGTH-1, MIN_USER_LENGTH-1, USER_REQUIRED_ALPHABET), "email" => "rafael.clp@hotmail.com"));
-		$user[2](array("username" => generate_random_string(MAX_USER_LENGTH+1, MAX_USER_LENGTH+1, USER_REQUIRED_ALPHABET), "email" => "rafael.clp@hotmail.com"));
+		$user[0](array("username" => generate_random_string(USER_MIN_LENGTH, USER_MIN_LENGTH, USER_OPTIONAL_ALPHABET), "email" => "rafael.clp@hotmail.com"));
+		$user[1](array("username" => generate_random_string(USER_MIN_LENGTH-1, USER_MIN_LENGTH-1, USER_REQUIRED_ALPHABET), "email" => "rafael.clp@hotmail.com"));
+		$user[2](array("username" => generate_random_string(USER_MAX_LENGTH+1, USER_MAX_LENGTH+1, USER_REQUIRED_ALPHABET), "email" => "rafael.clp@hotmail.com"));
 		$user[3](array("username" => "rafaclp", "email" => "rafael.clp"));
 		$user[4](array("username" => "rafaclp", "email" => "rafael.clpaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa@hotmail.com"));
 		$user[5](array("username" => "rafaelclp", "email" => "rafael.clp@hotmail.com"));
@@ -37,6 +39,7 @@ class UserTest
 		if (!assertNull($user[5]->id)) return;
 		if (!assertEquals($user[5]->register(), REGISTER_SUCCESS)) return;
 		if (!assertNotNull($user[5]->id)) return;
+		$this->rafaelclp_id = $user[5]->id;
 		if (!assertEquals($user[6]->register(), EMAIL_ALREADY_EXISTS)) return;
 		if (!assertEquals($user[7]->register(), USER_ALREADY_EXISTS)) return;
 		if (!assertEquals($user[8]->register(), REGISTER_SUCCESS)) return;
@@ -45,14 +48,48 @@ class UserTest
 
 	function login()
 	{
+		$user = new User();
+		if (!assertFalse($user->login("rafaelclp", "rafolo"))) return;
+		if (!assertTrue($user->login("rafaelclp", "rafael"))) return;
+		if (!assertTrue($user->login("rafael.clp@hotmail.com", "rafael"))) return;
+		if (!assertEquals($user->username, "rafaelclp")) return;
+		if (!assertEquals($user->email, "rafael.clp@hotmail.com")) return;
+		if (!assertFalse($user->login("rafael2.clp@hotmail.com", "rafael"))) return;
+		if (!assertFalse($user->login("12391212", "rafael"))) return;
 	}
 
 	function get()
 	{
+		$user = new User();
+		if (!assertFalse($user->get(0))) return;
+		if (!assertFalse($user->get("rafoloclp"))) return;
+		if (!assertFalse($user->get("rafolo.clp@hotmail.com"))) return;
+		if (!assertFalse($user->get("rafael.clp@hotmail.com"))) return;
+		if (!assertTrue($user->get("rafaelclp"))) return;
+		if (!assertEquals($user->id, $this->rafaelclp_id)) return;
+		if (!assertEquals($user->email, "rafael.clp@hotmail.com")) return;
+		if (!assertTrue($user->get($this->rafaelclp_id))) return;
+		if (!assertEquals($user->username, "rafaelclp")) return;
+		if (!assertEquals($user->email, "rafael.clp@hotmail.com")) return;
 	}
 
 	function update()
 	{
+		$user = new User();
+		$user->get("rafaelclp");
+		$user->password = "rafolete";
+		if (!assertTrue($user->update())) return;
+		if (!assertFalse($user->login("rafaelclp", "rafael"))) return;
+		if (!assertTrue($user->login("rafaelclp", "rafolete"))) return;
+		$user->email = "rafael";
+		if (!assertFalse($user->update())) return;
+		$user->email = "rafael.clp@hotmail.com";
+		if (!assertTrue($user->update())) return;
+		$user->username = "12182812";
+		if (!assertFalse($user->update())) return;
+		$user->username = "rafaelclp";
+		$user->password = generate_random_string(PASSWORD_MIN_LENGTH-1, PASSWORD_MIN_LENGTH-1, PASSWORD_ALPHABET);
+		if (!assertFalse($user->update())) return;
 	}
 
 	function remove()
